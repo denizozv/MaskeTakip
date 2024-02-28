9fd14c906443ebdd9ccc3cd18fc66d9408aa0b56
Deniz Özveren <115140344+denizozv@users.noreply.github.com>
2024-02-28 16:30:09 +0300

    Add files via upload

---
+++ Bussiness/Abstarct/IApplicantService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Abstarct
+{
+    public interface IApplicantService
+    {
+        void ApplyForMask();
+
+        List<Person> GetAll();
+
+
+        bool CheckPerson(Person person);
+
+    }
+}
---
+++ Bussiness/Concrete/PersonManager.cs
@@ -0,0 +1,32 @@
+﻿using Bussiness.Abstarct;
+using Entities.Concrete;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask()
+        {
+            
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(person.NationalIdentity, person.FirstName, person.LastName, person.DateOfBirthYear)
+                .Result.Body.TCKimlikNoDogrulaResult;
+        }
+
+        public List<Person> GetAll()
+        {
+            return null;
+        }
+    }
+}
