f24fb3ec344c54d1aeff7f0291c78f1a1807e2fc
Deniz Özveren <115140344+denizozv@users.noreply.github.com>
2024-02-28 16:32:56 +0300

    Add files via upload

---
+++ MaskeTakip/Program.cs
@@ -0,0 +1,22 @@
+﻿using Bussiness.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            FirstName = "DENİZ",
+            LastName = "ÖZVEREN",
+            DateOfBirthYear = 2002,
+            NationalIdentity = 6789
+        };
+        PttManager pttManager = new PttManager(new PersonManager());
+
+        pttManager.GiveMask(person);
+
+    }
+}
+
+
